4e2453c9d81cb11bee244a22c45125028d0b39a7
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-03-12 11:04:02 -0300

    Spawm Ball

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> prefabs;
+    [SerializeField] private Vector3 _originPoint = new Vector3(0, 15, -1);
+    [SerializeField] private float _spawmInterval = 1;
+    [SerializeField] private float _cooldown = 1;
+
+    [SerializeField] private GameManager gameManager;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _cooldown -= Time.deltaTime;
+        if (_cooldown <= 0f)
+        {
+            _cooldown = _spawmInterval;
+            SpawmBall();
+        }
+    }
+
+    private void SpawmBall()
+    {
+        int prefabIndex = UnityEngine.Random.Range(0, prefabs.Count);
+        GameObject prefab = prefabs[prefabIndex];
+
+
+        float gameWidth = GameManager.Instance.gameWidth;
+        float xOffset = UnityEngine.Random.Range(-gameWidth / 2f, gameWidth / 2f);
+        Vector3 position = _originPoint + new Vector3(xOffset, 0, 0);
+
+
+
+        Quaternion rotation = prefab.transform.rotation;
+
+        Instantiate(prefab, position, rotation);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 4f;
+    [SerializeField] private float speed = 10f;
     void Start()
     {
 
